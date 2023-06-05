9560cbd332d2c5235693daaa49efe8b16fab7f6c
thekarthikshet <shetkarthik8888@gmail.com>
2023-06-05 15:57:35 +0530

    first commit

---
+++ src/main/java/com/karth/practice/PracticeApplication.java
@@ -0,0 +1,19 @@
+package com.karth.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+		 int a =8;
+		 int b =80;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
